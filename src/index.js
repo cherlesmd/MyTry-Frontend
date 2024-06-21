bd1bd45d4b58c117dc0722bb4a4a14241f6b0aa5
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-06-20 19:03:19 -0700

    routes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
+import SignUp from "./components/signup/Sign-Up";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dash" element={<App />} />
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dash/*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
