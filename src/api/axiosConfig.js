ab00b613784b5f2ed5d0eddf23d578e0306c7281
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-07-21 18:22:16 -0700

    new site

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://mytrybackend-88bc8727b196.herokuapp.com/api/v1",
   timeout: 20000,
   withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://mytrybackend-88bc8727b196.herokuapp.com/api/v1",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
